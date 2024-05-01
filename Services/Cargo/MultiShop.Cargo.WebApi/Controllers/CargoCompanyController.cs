5a39d427de27423d90dbef08874089e7f9d87970
MSoykan <mehmetsoykanmutlu@gmail.com>
2024-05-01 14:53:43 +0300

    Cargo Service Business Layer&Controllers
    - Fixed the bug with the project folder being on an irrelevant folder
    - Added abstract services and their respective manager classes to business layer
    - Added CargoCompanyCustomer controller to presentation layer(web api) with crud operation

---
+++ Services/Cargo/MultiShop.Cargo.WebApi/Controllers/CargoCompanyController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Cargo.BusinessLayer.Abstract;
+using MultiShop.Cargo.DtoLayer.Dtos.CargoCompanyDtos;
+using MultiShop.Cargo.EntityLayer.Concrete;
+
+namespace MultiShop.Cargo.WebApi.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CargoCompanyController : ControllerBase {
+        private readonly ICargoCompanyService cargoCompanyService;
+
+        public CargoCompanyController(ICargoCompanyService cargoCompanyService) {
+            this.cargoCompanyService = cargoCompanyService;
+        }
+
+        [HttpGet]
+        public IActionResult CargoCompanyList() {
+            var values = cargoCompanyService.TGetAll();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCargoCompany(CreateCargoCompanyDto createCargoCompanyDto) {
+            CargoCompany cargoCompany = new CargoCompany() {
+                CargoCompanyName = createCargoCompanyDto.CargoCompanyName,
+            };
+            cargoCompanyService.TInsert(cargoCompany);  
+            return Ok("CargoCompany created succesfully.");
+        }
+        [HttpDelete]
+        public IActionResult RemoveCargoCompany(int id) {
+            cargoCompanyService.TDelete(id);
+            return Ok("CargoCompany deleted with success");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCargoCompanyById(int id) {
+            var values = cargoCompanyService.TGetById(id);
+            return Ok(values);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCargoCompany(UpdateCargoCompanyDto updateCargoCompanyDto) {
+            CargoCompany cargoCompany = new CargoCompany() {
+                CargoCompanyId = updateCargoCompanyDto.CargoCompanyId,
+                CargoCompanyName = updateCargoCompanyDto.CargoCompanyName,
+            };
+            cargoCompanyService.TUpdate(cargoCompany);
+            return Ok("CargoCompany updated with success");
+        }
+
+    }
+}
