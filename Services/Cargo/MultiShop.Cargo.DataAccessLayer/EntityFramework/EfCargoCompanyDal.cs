2efc9813a06c4652af41d366de090267ab6d384a
MSoykan <mehmetsoykanmutlu@gmail.com>
2024-05-01 13:29:13 +0300

    Working on Business and DataAccess Layers
    - Added abstract repos to data acces layer
    - Added concrete implementation of abstract repos to data access layer
    - Added Abstract services to business layer

---
+++ Services/Cargo/MultiShop.Cargo.DataAccessLayer/EntityFramework/EfCargoCompanyDal.cs
@@ -0,0 +1,18 @@
+﻿using MultiShop.Cargo.DataAccessLayer.Abstract;
+using MultiShop.Cargo.DataAccessLayer.Concrete;
+using MultiShop.Cargo.DataAccessLayer.Repositories;
+using MultiShop.Cargo.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.DataAccessLayer.EntityFramework {
+    public class EfCargoCompanyDal : GenericRepository<CargoCompany>, ICargoCompanyDal {
+        // aynı and neden cargo compant ve ICargoCompant interfacelerini implemente ediyoruz?
+        //ICargoCompanyDal içinde sadece cargocompany'e özgü işlemler barınır
+        public EfCargoCompanyDal(CargoContext context) : base(context) {
+        }
+    }
+}
